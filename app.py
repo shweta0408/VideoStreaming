d67762a3e39b0235a018779f053733fe8d122e9a
Shweta Mishra <shmis2004@gmail.com>
2025-05-05 23:02:17 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+
+# Create camera instance
+camera = cv2.VideoCapture(0)
+
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            # Encode the frame in JPEG format
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+            # Yield frame in byte format
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
